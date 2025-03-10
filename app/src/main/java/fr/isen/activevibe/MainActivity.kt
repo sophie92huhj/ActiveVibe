6745c40aaa24555bf4f91867ceaa6424757c6575
sophie92huhj <sophie.soubry@isen.yncrea.fr>
2025-03-10 08:43:29 +0100

    Initial Commit

--- app/src/main/java/fr/isen/activevibe/MainActivity.kt
+++ app/src/main/java/fr/isen/activevibe/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import fr.isen.activevibe.ui.theme.ActiveVibeTheme
 
+//COUCOU
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +25,7 @@ class MainActivity : ComponentActivity() {
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
-                }
+
             }
         }
     }
